edf20e280169ae50976d70d60bf2910145f1b1ec
Sung Hwan AHN <superahn@gmail.com>
2013-02-07 13:32:16 +1100

    no message

--- functions.php
+++ functions.php
@@ -12,14 +12,40 @@ function sample_viewport_meta_tag() {
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
 }
 
+// Add new image sizes (for such as featured images or grid thumbnails)
+add_image_size('grid-thumbnail', 100, 100, TRUE);
+
 // Add support for custom background
 add_theme_support( 'custom-background' );
 
+//---- HEADER ----
 // Add support for custom header
 add_theme_support( 'genesis-custom-header', array(
 	'width' => 1152,
 	'height' => 120
 ) );
 
+// Reposition the Primary Navigation
+remove_action('genesis_after_header', 'genesis_do_nav');
+add_action('genesis_header_right', 'genesis_do_nav');	// will reposition navigation
+
+/** Remove the header right widget area */
+unregister_sidebar( 'header-right' );		// will remove search form
+
+
+//---- FOOTER ----
 // Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+// Genesis footer
+
+/** Customize the credits */
+add_filter( 'genesis_footer_creds_text', 'custom_footer_creds_text' );
+function custom_footer_creds_text() {
+    $creds = '<div class="creds"><p>';
+    $creds .=  'Copyright &copy; ';
+    $creds .=  date( 'Y' );
+    $creds .=  ' &middot; <a href="http://webappdev.net">WEB & MOBILE DEVELOPER</a> &middot; Sung Ahn';
+    $creds .=  '</p></div>';
+    return $creds;
+}
--- home.php
+++ home.php
@@ -10,7 +10,7 @@ add_action('genesis_loop', 'child_grid_loop_helper');
 function child_grid_loop_helper() {
 	if(function_exists('genesis_grid_loop')) {
 		genesis_grid_loop( array(
-			'features' => 2,
+			'features' => 0,
 			'feature_image_size' => 0,
 			'feature_image_class' => 'alignleft post-image',
 			'feature_content_limit' => 0,
@@ -26,5 +26,14 @@ function child_grid_loop_helper() {
 
 // Remove the post meta function for front page only
 remove_action('genesis_after_post_content', 'genesis_post_meta');
+
+// Intro
+add_action('genesis_after_header', 'intro_helper');
+
+function intro_helper() {
+	echo '<div id="intro">';
+	echo '<h1>I am a <strong>Melbourne based Web Developer</strong><br>specialising in <strong>Web Apps and Mobile development.</strong></h1>';	
+	echo '</div>';
+}
 
 genesis();
